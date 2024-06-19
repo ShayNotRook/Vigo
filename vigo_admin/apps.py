6e3bfe47befba49b7c57a0ed1da15c935666cc14
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-19 15:43:37 +0330

    Added vigo_admin app.

---
+++ vigo_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VigoAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vigo_admin'
--- vigo_shop/settings.py
+++ vigo_shop/settings.py
@@ -139,7 +139,7 @@ class Dev(Configuration):
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
     
-    GAME_COVER_URL = os.path.join(MEDIA_ROOT,)
+    GAME_COVERS_DIR = os.path.join(MEDIA_ROOT, 'game_covers')
 
     # Default primary key field type
     # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
