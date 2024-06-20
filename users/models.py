e6f59f5b17c5ad3ef4a8e42ab7ab596d6ec81a7a
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-20 17:17:00 +0330

    Added USER_LEVEL for User model.

--- users/models.py
+++ users/models.py
@@ -2,5 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 class User(AbstractUser):
-    pass
+    # User status settings (Choices)
+    USER_LEVELS = (
+        ('BASIC', 'Bronze'),
+        ('REGULAR', 'Silver'),
+        ('EXPERT', 'Gold'),
+        ('ULTIMATE', 'Platinum')
+    )
+    
+    status = models.CharField(choices=USER_LEVELS, max_length=50)
