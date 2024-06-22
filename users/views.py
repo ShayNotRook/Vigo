c50981778ea3ba78159d693512584ceb33db08e8
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-22 18:44:32 +0330

    Added simple profile_view, and profile_view template.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+
+User = get_user_model()
+
+@login_required
+def profiel(request):
+    user = request.user
+    return render(request, 'users/profile_view.html', {'user': user})
