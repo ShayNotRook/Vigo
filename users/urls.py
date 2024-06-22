c8cd6ee73e7eb9702c13a984f6bd871b37e4463c
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-23 01:26:48 +0330

    Added Custom logout view and url.

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('profile/', user_views.profile, name='profile_view'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', user_views.CustomLogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
 
+from django.views import View
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model, logout
+
+from django.utils.decorators import method_decorator
+
+from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 
 # User = get_user_model()
@@ -10,3 +16,14 @@ from django.contrib.auth import get_user_model
 def profile(request):
     user = request.user
     return render(request, 'profile_view.html', {'user': user})
+
+
+
+class CustomLogoutView(View):
+    @method_decorator(csrf_protect)
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('login')
+    
+    def get(self, request, *args, **kwargs):
+        return redirect('profile') # Redirect to a profile if accessed via GET
