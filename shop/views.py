d1f3c61dfc512ed83ab9f23bbf549473cd92bbd7
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-25 17:48:39 +0330

    Fixed an issue where home page only returned the user model not the request.user property of the authenticated user.

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
+
 
 # Create your views here.
 def ShopHome(request):
     welcome_test = "Welcome to Vigo!"
-    user = get_user_model()
-    return render(request, 'home.html', {'welcome_text': welcome_test, 'user': user})
+    return render(request, 'home.html', {'welcome_text': welcome_test})
 
 
 def ContactView(request):
