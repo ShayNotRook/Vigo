55506a8f15989906157afee786b54936c750f77f
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-28 18:32:13 +0330

    Added Cart and CartItem models, Built relations to Products model using ContentType framework.

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
+User = get_user_model()
 # Create your models here.
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.user.username}"
+    
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    quantity = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.quantity}"
+
+    def get_total_price(self):
+        return self.quantity * self.content_object.price
