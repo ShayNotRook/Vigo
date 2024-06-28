fee9acd05cc6c7b22aeb87aa0d9ea1d9b540156a
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-06-28 18:54:56 +0330

    Minor fix, as well as testing admin panel for Cart and CartItem model admins.

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Cart, CartItem
 # Register your models here.
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'updated_at')
+    
+    
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'get_content_object_name', 'quantity', 'get_total_price')
+    readonly_fields = ('get_total_price',)
+    
+    def get_total_price(self, obj):
+        return obj.get_total_price()
+    get_total_price.short_description = 'Total Price'
+    
+    def get_content_object_name(self, obj):
+        return obj.content_object.name
+    get_content_object_name.short_description = 'Product'
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
