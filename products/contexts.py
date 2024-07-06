a4af380d5ecda0c65350a7b5769e999ac473c3ff
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-07-07 01:58:32 +0330

    Added more style guide, adding a shadow overlay while burger menu is active.

--- products/contexts.py
+++ products/contexts.py
@@ -1,5 +1,5 @@
 from .models import Category
 
 def top_level_categories(request):
-    categories = Category.objects.filter(parent__isnull=True)
+    categories = Category.objects.filter(parent__isnull=True).prefetch_related('subcategories')
     return {'top_level_categories': categories}
--- static/frontend/script.js
+++ static/frontend/script.js
@@ -13,37 +13,60 @@ document.addEventListener('DOMContentLoaded', function() {
     // Hamburger Menu Toggle
     const menuToggle = document.getElementById('menu-toggle');
     const menuContent = document.getElementById('menu-content');
+    const overlay = document.getElementById('overlay');
 
     menuToggle.addEventListener('click', function() {
         menuContent.classList.toggle('active');
+        overlay.classList.toggle('active');
     });
+
+    // Close the menu when clicking the overlay
+    overlay.addEventListener('click', function() {
+        menuContent.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
     // Function to handle category click
-    function handleCategoryClick(event) {
-        const categoryId = event.target.dataset.id; // Get the category ID\
-        const url = '/category/${categoryId}/subcategories/'; // URL to fetch subcategories
+    // function handleCategoryClick(event) {
+    //     const categoryId = event.target.dataset.id; // Get the category ID\
+    //     const url = '/category/${categoryId}/subcategories/'; // URL to fetch subcategories
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const subcategoryList = document.getElementById('subcategory-list');
-                subcategoryList.innerHTML = ''; // Clear any existing subcategories
-                data.forEach(subcategory => {
-                    var listItem = document.createElement('li');
-                    listItem.textContent = subcategory.name;
-                    subcategoryList.appendChild(listItem);
-                });
-            })
-            .catch(error => {
-                console.error('Error', error);
-                alert('An error occured while loading subcategories');  
-            });
+    //     fetch(url)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             const subcategoryList = document.getElementById('subcategory-list');
+    //             subcategoryList.innerHTML = ''; // Clear any existing subcategories
+    //             data.forEach(subcategory => {
+    //                 var listItem = document.createElement('li');
+    //                 listItem.textContent = subcategory.name;
+    //                 subcategoryList.appendChild(listItem);
+    //             });
+    //         })
+    //         .catch(error => {
+    //             console.error('Error', error);
+    //             alert('An error occured while loading subcategories');  
+    //         });
 
-    }
+    // }
 
-    // Attach event listeners to all top-level category items
-    var categoryItems = document.querySelectorAll('.top-level-category');
-    categoryItems.forEach(item => {
-            item.addEventListener('click', handleCategoryClick);
+    // // Attach event listeners to all top-level category items
+    // var categoryItems = document.querySelectorAll('.top-level-category');
+    // categoryItems.forEach(item => {
+    //         item.addEventListener('click', handleCategoryClick);
+    // });
+
+    // Subcategory Toggle
+    const topLevelCategories = document.querySelectorAll('.top-level-category');
+
+    topLevelCategories.forEach(category => {
+        category.addEventListener('click', function(event) {
+            event.preventDefault();
+            const subcategories = this.nextElementSibling;
+            if (subcategories) {
+                subcategories.classList.toggle('active');
+                subcategories.style.display = subcategories.style.display === 'block' ? 'none': 'block';
+            }
+        });
     });
 });
 
