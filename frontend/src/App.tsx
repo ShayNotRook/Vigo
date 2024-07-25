ded5713fe1c23e723c14382ef3e239fdd6212a32
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-07-25 16:54:21 +0330

    Re Installed React using Vite, Fixed earlier vulnerablities.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,35 @@
-// import logo from './logo.svg';
-import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
-// import ItemDetail from './components/product/itemDetail';
-// import ItemList from './components/product/itemList';
-import ListGroup from './components/tests/ListGroup';
-import './App.css';
-import Alert from './components/tests/Alert';
-import Button from './components/tests/Buttons';
-// import Footer from './components/base/Footer'
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  // let items = ['Chicago', 'New York', 'Cairo', 'Toronto']
-
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // }
-  const [alertVisible, setAlertVisible] = useState(false);
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      { alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert> }
-
-      <Button onClick={() => setAlertVisible(true)}>My Button</Button>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
